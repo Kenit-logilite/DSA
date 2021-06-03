2244a0b99225b20888610232ef399b38d959b937
Kenit-logilite <85211904+Kenit-logilite@users.noreply.github.com>
2021-06-03 13:50:31 +0530

    QueueExample By LinkedList

---
+++ DSA Problems/src/Array/QueueByLinkedList.java
@@ -0,0 +1,72 @@
+package Array;
+
+import Array.QueueByLinkedList.Node;
+
+public class QueueByLinkedList {
+	private Node front,rear;
+	public int size;
+	class Node
+	{
+		public int data;
+		public Node next;
+		
+	}
+	
+	
+	public QueueByLinkedList() {
+	
+		 front = null;
+		 rear =null;
+		 size= 0;
+	}
+
+	public boolean isEmpty()
+	{
+		return(size==0);
+	}
+	
+	public int deQueue()
+	{
+		int data = front.data;
+		front = front.next;
+		if (isEmpty()) 
+		 {
+		 rear = null;
+		 }
+		size--;
+		System.out.println(data+ " removed from the queue");
+		return data;
+	}
+	
+	public void enQueue(int data)
+	{
+		Node Rrear = rear;
+		rear = new Node();
+		rear.data = data;
+		rear.next=null;
+				if (isEmpty()) 
+				 {
+				 front = rear;
+				 }
+				 else 
+				 {
+				 Rrear.next = rear;
+				 }
+				 size++;
+				 System.out.println(data);
+	}
+	
+	
+
+	public static void main(String[] args) {
+		QueueByLinkedList q = new QueueByLinkedList();
+		q.enQueue(10);
+		q.enQueue(20);
+		q.enQueue(30);
+		q.enQueue(40);
+		q.enQueue(50);
+		q.deQueue();
+		
+	}
+
+}

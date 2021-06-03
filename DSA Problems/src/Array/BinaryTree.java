3ed59169a886a2e5ea5e740d06c661c29af4546c
Kenit-logilite <85211904+Kenit-logilite@users.noreply.github.com>
2021-06-03 15:38:28 +0530

    Binary Tree Example

---
+++ DSA Problems/src/Array/BinaryTree.java
@@ -0,0 +1,36 @@
+package Array;
+
+class Node {
+	  int key;
+	  Node left, right;
+
+	  public Node(int data) {
+	  key = data;
+	  left = right = null;
+	  }
+	}
+
+	class BinaryTree {
+	  Node root;
+
+	  public void traverseTree(Node node) {
+	    if (node != null) {
+	      traverseTree(node.left);
+	      System.out.print(" " + node.key);
+	      traverseTree(node.right);
+	    }
+	  }
+
+	  public static void main(String[] args) {
+
+
+	    BinaryTree tree = new BinaryTree();
+	    tree.root = new Node(1);
+	    tree.root.left = new Node(2);
+	    tree.root.right = new Node(3);
+	    tree.root.left.left = new Node(4);
+
+	    System.out.print("Binary Tree: ");
+	    tree.traverseTree(tree.root);
+	  }
+	}
